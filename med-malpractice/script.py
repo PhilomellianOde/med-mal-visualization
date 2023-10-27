dd6a446fe3a03d5dd5e0dcf21f24d9449b97d2ed
PhilomellianOde <piazzajacob23@gmail.com>
2023-10-27 14:06:49 -0700

    Set up Mercury; added graphs

--- med-malpractice/script.py
+++ med-malpractice/script.py
@@ -1,11 +1,22 @@
 import numpy as np
 import pandas as pd
-import seaborn as sn
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 print(np.random.randint(1,9))
 
 # Read in Medical Malpractice CSV data
-med_data = pd.read_csv("data/raw-data/medicalmalpractice.csv")
+df = pd.read_csv("data/raw-data/medicalmalpractice.csv")
 
 
-print(med_data.head())
+print(df.head())
+
+print(df.head())
+
+print(df['Insurance'].value_counts())
+
+
+
+# Let's take a look at how claims are distributed. We'll take a look at the shape of the distribution.
+sns.histplot(data=df, x = "Amount", kde = True)
+plt.show()
